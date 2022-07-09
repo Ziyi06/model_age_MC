43c743204c5ce1ab6cc2ef98422aea5ed398e1f0
Ziyi Zhu <66013698+Ziyi06@users.noreply.github.com>
2022-07-09 22:29:17 +1000

    Add files via upload

--- bootstrap.py
+++ bootstrap.py
@@ -1,18 +1,20 @@
 
 import const as c
+
 import numpy as np
 
-def _generate_Hf(mother_sample, fixed=None):
+
+def generate_Hf(mother_sample, fixed=None):
     return np.random.choice(mother_sample) if fixed is None else fixed
 
-def _Hf2Lu(gen_Hf):
-    chur_hh_init = _extrapolate(4.56, c.chur_hh, c.chur_lh)
+def Hf2Lu(gen_Hf):
+    chur_hh_init = extrapolate(4.56, c.chur_hh, c.chur_lh)
     return (gen_Hf - chur_hh_init) / (np.exp(c.half_life * 4.56) - 1)
 
-def _lin_intrapolate(x, y1, y2, turn_points=c.turn_points):
+def lin_intrapolate(x, y1, y2, turn_points=c.turn_points):
     return ((y2 - y1)*x + (turn_points[1]*y1 - turn_points[0]*y2)) / np.ptp(turn_points)
 
-def _get_crust_Lu(oxy, y1_tuple=c.y1_tuple, y2_tuple=c.y2_tuple, ave_tuple=c.ave_tuple,
+def get_crust_Lu(oxy, y1_tuple=c.y1_tuple, y2_tuple=c.y2_tuple, ave_tuple=c.ave_tuple,
            Lu_crust_err=True):
     oxy_arr = np.ones(1) * oxy if type(oxy) == float else oxy
     low_branch = np.random.normal(y1_tuple[0], y1_tuple[1]*Lu_crust_err, len(oxy_arr))
@@ -27,19 +29,19 @@ def _get_crust_Lu(oxy, y1_tuple=c.y1_tuple, y2_tuple=c.y2_tuple, ave_tuple=c.ave
     nan_ind = np.isnan(oxy_arr)
     # For the middle branch, bootstrapping module has already been built.
     # Directly call lin_intrapolate function will automatically use it.
-    mid_branch = _lin_intrapolate(oxy_arr, np.random.normal(y1_tuple[0], y1_tuple[1] * Lu_crust_err),
+    mid_branch = lin_intrapolate(oxy_arr, np.random.normal(y1_tuple[0], y1_tuple[1] * Lu_crust_err),
                                  np.random.normal(y2_tuple[0], y2_tuple[1] * Lu_crust_err))
     res_arr = (np.where(low_ind, low_branch, 0.) + np.where(high_ind, high_branch, 0.) +
         np.where(nan_ind, nan_branch, 0.) + np.where(~low_ind & ~high_ind & ~nan_ind, mid_branch, 0.))
     return float(res_arr) if type(oxy) == float else res_arr
 
-def _extrapolate(delta_t, Hf, Lu):
+def extrapolate(delta_t, Hf, Lu):
     return Hf - Lu * (np.exp(c.half_life * (delta_t)) - 1)
 
-def _epsilon(t, Hf, Lu):
-    return (_extrapolate(t, Hf, Lu) / _extrapolate(t, c.chur_hh, c.chur_lh) - 1) * 1e4
+def epsilon(t, Hf, Lu): 
+    return (extrapolate(t, Hf, Lu) / extrapolate(t, c.chur_hh, c.chur_lh) - 1) * 1e4
 
-def _solve_T(t, Hf_arr, Lu_arr,
+def solve_T(t, Hf_arr, Lu_arr,
             Hf_am=c.am_hh, Lu_am=c.am_lh, Lu_crustal=0.021, method='direct'):
     '''
     Parameters
@@ -49,8 +51,8 @@ def _solve_T(t, Hf_arr, Lu_arr,
     '''
     # Method1: get Hf_am_T by T-> t
     if method != 'direct':
-        Hf_sample_t = _extrapolate(t, Hf_arr, Lu_arr)
-        Hf_am_t = _extrapolate(t, Hf_am, Lu_am)
+        Hf_sample_t = extrapolate(t, Hf_arr, Lu_arr)
+        Hf_am_t = extrapolate(t, Hf_am, Lu_am)
         delta_Hf = Hf_sample_t - Hf_am_t
         delta_Lu = Lu_crustal - (Lu_am * np.exp(c.half_life * t))
         T = t + 1/c.half_life * np.log(1 + delta_Hf / delta_Lu)
@@ -58,14 +60,47 @@ def _solve_T(t, Hf_arr, Lu_arr,
         return np.where(t > T, t, T)
     # Method2: get Hf_am_T by T-> 0
     else:
-        Hf_sample_t = _extrapolate(t, Hf_arr, Lu_arr)
+        Hf_sample_t = extrapolate(t, Hf_arr, Lu_arr)
         T = 1/c.half_life * np.log((np.exp(c.half_life * t) * 
             (Hf_am + Lu_am - Hf_sample_t - Lu_crustal)) / 
             (np.exp(c.half_life * t) * Lu_am - Lu_crustal) )
         return np.where(t > T, t, T)
 
+def T_boot(t, Hf_arr, Lu_arr, oxy_arr,
+          mother_sample=None, percentiles=(2.5, 97.5)):
+    matrix = []
+    for i in range(100):
+        Hf_am = generate_Hf(mother_sample)
+        Lu_am = Hf2Lu(Hf_am)
+        matrix.append(solve_T(t, Hf_arr, Lu_arr, oxy_arr, Hf_am=Hf_am, Lu_am=Lu_am))
+
+    print("Median age is ", np.percentile(matrix, 50, axis=0))
+    print(percentiles[0], "% age is", np.percentile(matrix, percentiles[0], axis=0))
+    print(percentiles[1], "% age is", np.percentile(matrix, percentiles[1], axis=0))
+
+
+def calc_epsilon_error(data, times=100, meas_err=1, percentiles=(2.5, 97.5)):
+    '''
+    Calcuate error of epsilon_hf based on measurement errors
+    '''
+    t0, Hf0, Lu0 = data.u_pb_age / 1e3, data.hf_hf, data.lu_hf
+    t_err = data.age_2se / 1e3 / 2
+    Hf_err, Lu_err = data.hf_hf_2se / 2, data.lu_hf_2se / 2
+
+    epsilon_list = []
+    for i in range(times):
+        t = np.random.normal(t0, t_err * meas_err)
+        Hf = np.random.normal(Hf0, Hf_err * meas_err)
+        Lu = np.random.normal(Lu0, Lu_err * meas_err)
+        epsilon_hf = epsilon(t, Hf, Lu)
+        epsilon_list.append(epsilon_hf)
+        
+    return (np.percentile(epsilon_list, 50, axis=0),
+            np.percentile(epsilon_list, percentiles[0], axis=0),
+            np.percentile(epsilon_list, percentiles[1], axis=0))
+
 def calc_T(data, mother_sample, times=5000, percentiles=(2.5, 97.5), 
-           meas_err=True, Hf_am_err=True, Lu_crust_err=True):
+           meas_err=True, Hf_am_err=True, Lu_crust_err=True, fixed_Lu=False):
     t0, Hf0 = data.u_pb_age / 1e3, data.hf_hf
     Lu0, oxy0 = data.lu_hf, data.o
     t_err, Hf_err = data.age_2se / 1e3 / 2, data.hf_hf_2se / 2
@@ -79,13 +114,18 @@ def calc_T(data, mother_sample, times=5000, percentiles=(2.5, 97.5),
         oxy = np.random.normal(oxy0, oxy_err * meas_err)
 
         fixed = None if Hf_am_err else c.am_hh
-        Hf_am = _generate_Hf(mother_sample, fixed=fixed)
-        Lu_am = _Hf2Lu(Hf_am)
-        Lu_crustal = _get_crust_Lu(oxy, Lu_crust_err=Lu_crust_err)
+        Hf_am = generate_Hf(mother_sample, fixed=fixed)
+        Lu_am = Hf2Lu(Hf_am)
 
-        T = _solve_T(t, Hf, Lu, Hf_am=Hf_am, Lu_am=Lu_am, Lu_crustal=Lu_crustal)
+        if fixed_Lu:
+            Lu_crustal = np.where(t <= 2.5, c.ave_tuple[0], 0.) + np.where(t > 2.5, c.y1_tuple[0], 0.)
+        else:
+            Lu_crustal = get_crust_Lu(oxy, Lu_crust_err=Lu_crust_err)
+
+        T = solve_T(t, Hf, Lu, Hf_am=Hf_am, Lu_am=Lu_am, Lu_crustal=Lu_crustal)
         T_list.append(T)
 
     return (np.percentile(T_list, 50, axis=0),
             np.percentile(T_list, percentiles[0], axis=0),
             np.percentile(T_list, percentiles[1], axis=0))
+
--- const.py
+++ const.py
@@ -3,17 +3,21 @@ mantle_o = (5.3, 0.6) # 2SD
 
 half_life = 0.01867  # in unit of Gyr^{-1}
 
-chur_lh = 0.0336
+chur_lh = 0.0336 ## From Bouvier et al. (2008)
 chur_hh = 0.282785
 
-dm_lh = 0.0384
-dm_hh = 0.283250
+# chur_lh = 0.0332 ## From Blichert-Toft and Albarède, 1997
+# chur_hh = 0.282772
+
+dm_lh = 0.0384 
+dm_hh = 0.283250  ## From Griffin 2002
 
 am_hh = 0.283160
 am_lh = 0.0378
 
-turn_points = (6.5, 10.)      # Low & high oxygen boundaries
-y1_tuple = (0.0220, 0.0017)   # Lu/Hf when oxygen < 6.5
+turn_points = (6.5, 10.)  # Low & high oxygen boundaries
+# y1_tuple = (0.0215, 0.0005)   # Lu/Hf when oxygen < 6.5
+y1_tuple = (0.0220, 0.0017)
 y2_tuple = (0.0083, 0.0017)   # Lu/Hf when oxygen > 10
 ave_tuple = (0.0115, 0.)
 
