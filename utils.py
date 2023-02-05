03368b496fdc2fef2d4e90754a43e6199eb0fbfa
Ziyi Zhu <66013698+Ziyi06@users.noreply.github.com>
2023-02-05 15:56:16 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def lin_intrapolate(x, y1, y2, turn_points=c.turn_points):
     return ((y2 - y1)*x + (turn_points[1]*y1 - turn_points[0]*y2)) / np.ptp(turn_points)
 
 def get_crust_Lu(oxy, y1_tuple=c.y1_tuple, y2_tuple=c.y2_tuple, ave_tuple=c.ave_tuple,
-                 Lu_crust_err=True):
+           Lu_crust_err=True):
     oxy_arr = np.ones(1) * oxy if type(oxy) == float else oxy
     low_branch = np.random.normal(y1_tuple[0], y1_tuple[1]*Lu_crust_err, len(oxy_arr))
     # Create an array whose length is indentical to oxy_arr.
@@ -35,7 +35,6 @@ def get_crust_Lu(oxy, y1_tuple=c.y1_tuple, y2_tuple=c.y2_tuple, ave_tuple=c.ave_
         np.where(nan_ind, nan_branch, 0.) + np.where(~low_ind & ~high_ind & ~nan_ind, mid_branch, 0.))
     return float(res_arr) if type(oxy) == float else res_arr
 
-
 def epsilon(t, Hf, Lu): 
     return (extrapolate(t, Hf, Lu) / extrapolate(t, c.chur_hh, c.chur_lh) - 1) * 1e4
 
@@ -47,3 +46,6 @@ def solve_T(u_pb_age, hf_hf, lu_hf,
         (Hf_am + Lu_am - Hf_sample_t - Lu_crustal)) / 
         (np.exp(c.half_life * u_pb_age) * Lu_am - Lu_crustal) )
     return np.where(u_pb_age > T, u_pb_age, T)
+
+
+
